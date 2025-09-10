449305a57244a228d5ef1a426a3ea37bb6bc0e13
a5632645 <3022602213@qq.com>
2025-09-10 21:10:32 +0800

    更新重采样的警告和内存分配

--- qwqdsp/include/qwqdsp/fx/resample.hpp
+++ qwqdsp/include/qwqdsp/fx/resample.hpp
@@ -20,27 +20,26 @@ public:
         phase_inc_ = source_fs / target_fs;
         kernel_len_ = kernel_len;
 
-        kernel_.resize(kernel_len * kOversample);
+        kernel_.resize(kernel_len * kOversample + 2, 0.0f);
         float const beta = qwqdsp::window::Kaiser::Beta(atten);
         float const width = qwqdsp::window::Kaiser::MainLobeWidth(beta) * std::numbers::pi_v<float> * 2.0f / static_cast<float>(kernel_len);
-
+        
+        std::span kernel_block{kernel_.data(), kernel_len * kOversample};
         if (target_fs < source_fs) {
             float const cutoff = std::numbers::pi_v<float> * target_fs / source_fs - width;
-            qwqdsp::filter::WindowFIR::Lowpass(kernel_, cutoff / kOversample);
+            qwqdsp::filter::WindowFIR::Lowpass(kernel_block, cutoff / kOversample);
         }
         else {
             float const cutoff = std::numbers::pi_v<float> - width;
-            qwqdsp::filter::WindowFIR::Lowpass(kernel_, cutoff / kOversample);
+            qwqdsp::filter::WindowFIR::Lowpass(kernel_block, cutoff / kOversample);
         }
-        
-        for (auto& s : kernel_) {
+
+        for (auto& s : kernel_block) {
             s *= kOversample;
         }
 
-        qwqdsp::window::Kaiser::ApplyWindow(kernel_, beta, false);
-        std::reverse(kernel_.begin(), kernel_.end());
-        kernel_.push_back(0);
-        kernel_.push_back(0);
+        qwqdsp::window::Kaiser::ApplyWindow(kernel_block, beta, false);
+        std::reverse(kernel_block.begin(), kernel_block.end());
     }
 
     std::vector<float> Process(std::span<float> x) noexcept {
@@ -49,7 +48,7 @@ public:
         float phase = 0.0f;
         int const ikernel_len = static_cast<int>(kernel_len_);
         int const xsize = static_cast<int>(x.size());
-        int xrpos = -(kernel_len_ - 1) / 2;
+        int xrpos = -(static_cast<int>(kernel_len_) - 1) / 2;
 
         while (xrpos < xsize) {
             float const frac = 1.0f - phase;
@@ -59,16 +58,16 @@ public:
             int const xibegin = std::max(0, frac == 0.0f ? xrpos : xrpos + 1);
             int const len = std::min(ikernel_len, xsize - xibegin);
             for (int i = begin; i < len; ++i) {
-                float const krpos = (i + frac) * kOversample;
+                float const krpos = (static_cast<float>(i) + frac) * kOversample;
                 size_t const ikrpos = static_cast<size_t>(krpos);
                 float const frac_krpos = krpos - std::floor(krpos);
                 float const kernel_v = qwqdsp::Interpolation::Linear(kernel_[ikrpos], kernel_[ikrpos + 1], frac_krpos);
-                sum += kernel_v * x[xibegin + i];
+                sum += kernel_v * x[static_cast<size_t>(xibegin + i)];
             }
             r.push_back(sum);
 
             phase += phase_inc_;
-            xrpos += std::floor(phase);
+            xrpos += static_cast<int>(std::floor(phase));
             phase = phase - std::floor(phase);
         }
 
