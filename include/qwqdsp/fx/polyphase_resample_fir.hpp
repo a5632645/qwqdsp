524e709b543c797896f7a72b3239bb80bf7c0cf6
a5632645 <3022602213@qq.com>
2026-06-25 00:11:17 +0800

    refactor: 合并polyphase到resample，修复polyphase downsample输出混叠

---
+++ include/qwqdsp/fx/polyphase_resample_fir.hpp
@@ -0,0 +1,189 @@
+#pragma once
+#include <vector>
+#include <span>
+#include <cassert>
+
+namespace qwqdsp_fx {
+// ------------------------------------------------------------
+// downsample
+// ------------------------------------------------------------
+class PolyphaseDownsamplerFir {
+public:
+    void Init(std::span<float> coeff, int downsample) {
+        downsample_ = downsample;
+
+        int len_coeff = static_cast<int>(coeff.size());
+        each_phase_len_ = len_coeff / downsample;
+        if (len_coeff % downsample != 0) {
+            ++each_phase_len_;
+        }
+        
+        phase_coeffs_.resize(downsample * each_phase_len_);
+        for (int i = 0; i < downsample; ++i) {
+            // 反转子滤波器的求值顺序
+            auto dst_it = phase_coeffs_.begin() + (downsample - i - 1) * each_phase_len_;
+            for (int j = 0; j < each_phase_len_; ++j) {
+                int idx = j * downsample + i;
+                if (idx < coeff.size()) {
+                    *dst_it = coeff[idx];
+                }
+                else {
+                    *dst_it = 0;
+                }
+                ++dst_it;
+            }
+        }
+
+        phase_state_.resize(phase_coeffs_.size());
+    }
+
+    void Reset() noexcept {
+        std::fill(phase_state_.begin(), phase_state_.end(), 0);
+    }
+
+    float Tick(std::span<float> x) noexcept {
+        assert(x.size() == downsample_);
+
+        float y = 0;
+        for (int i = 0; i < downsample_; ++i) {
+            auto* lag_it = phase_state_.data() + i * each_phase_len_;
+            auto* coeff_it = phase_coeffs_.data() + i * each_phase_len_;
+            y += TransposeFir(lag_it, coeff_it, x[i], each_phase_len_);
+        }
+        return y;
+    }
+
+    std::vector<float> Process(std::span<float> x) {
+        std::vector<float> out;
+
+        int full_loop_count = x.size() / downsample_;
+        int scalar_count = x.size() - full_loop_count * downsample_;
+        out.reserve(scalar_count == 0 ? full_loop_count : full_loop_count + 1);
+
+        for (int block = 0; block < full_loop_count; ++block) {
+            int const block_start = block * downsample_;
+            float y = 0;
+            for (int j = 0; j < downsample_; ++j) {
+                auto* lag_it = phase_state_.data() + j * each_phase_len_;
+                auto* coeff_it = phase_coeffs_.data() + j * each_phase_len_;
+                y += TransposeFir(lag_it, coeff_it, x[block_start + j], each_phase_len_);
+            }
+            out.push_back(y);
+        }
+
+        if (scalar_count != 0) {
+            int const block_start = full_loop_count * downsample_;
+            float y = 0;
+            for (int j = 0; j < scalar_count; ++j) {
+                auto* lag_it = phase_state_.data() + j * each_phase_len_;
+                auto* coeff_it = phase_coeffs_.data() + j * each_phase_len_;
+                y += TransposeFir(lag_it, coeff_it, x[block_start + j], each_phase_len_);
+            }
+            for (int j = scalar_count; j < downsample_; ++j) {
+                auto* lag_it = phase_state_.data() + j * each_phase_len_;
+                auto* coeff_it = phase_coeffs_.data() + j * each_phase_len_;
+                y += TransposeFir(lag_it, coeff_it, 0, each_phase_len_);
+            }
+            out.push_back(y);
+        }
+
+        return out;
+    }
+private:
+    static float TransposeFir(float* lag, float const* coeff, float x, int len) noexcept {
+        float const y = coeff[0] * x + lag[0];
+        for (size_t i = 0; i < len - 1; ++i) {
+            lag[i] = lag[i + 1] + coeff[i + 1] * x;
+        }
+        lag[len - 1] = coeff[len - 1] * x;
+        return y;
+    }
+
+    int downsample_{};
+    int each_phase_len_{};
+    std::vector<float> phase_coeffs_;
+    std::vector<float> phase_state_;
+};
+
+// ------------------------------------------------------------
+// upsample
+// ------------------------------------------------------------
+class PolyphaseUpsamplerFir {
+public:
+    void Init(std::span<float> coeff, int upsample) {
+        upsample_ = upsample;
+
+        int len_coeff = static_cast<int>(coeff.size());
+        each_phase_len_ = len_coeff / upsample;
+        if (len_coeff % upsample != 0) {
+            ++each_phase_len_;
+        }
+
+        phase_coeffs_.resize(upsample * each_phase_len_);
+        for (int i = 0; i < upsample; ++i) {
+            auto dst_it = phase_coeffs_.begin() + i * each_phase_len_;
+            for (int j = 0; j < each_phase_len_; ++j) {
+                int idx = j * upsample + i;
+                if (idx < coeff.size()) {
+                    *dst_it = coeff[idx] * upsample;
+                }
+                else {
+                    *dst_it = 0;
+                }
+                ++dst_it;
+            }
+        }
+
+        phase_state_.resize(each_phase_len_);
+    }
+
+    void Reset() noexcept {
+        std::fill(phase_state_.begin(), phase_state_.end(), 0);
+    }
+
+    void Tick(float x, std::span<float> y) noexcept {
+        assert(y.size() == upsample_);
+
+        PushFirState(x);
+        for (int i = 0; i < upsample_; ++i) {
+            auto* coeff_it = phase_coeffs_.data() + i * each_phase_len_;
+            y[i] = Fir(coeff_it);
+        }
+    }
+
+    std::vector<float> Process(std::span<float> x) {
+        std::vector<float> out;
+        out.reserve(x.size() * upsample_);
+
+        for (auto s : x) {
+            PushFirState(s);
+            for (int i = 0; i < upsample_; ++i) {
+                auto* coeff_it = phase_coeffs_.data() + i * each_phase_len_;
+                out.push_back(Fir(coeff_it));
+            }
+        }
+
+        return out;
+    }
+private:
+    float Fir(float const* coeff) noexcept {
+        float sum = 0;
+        for (int i = 0; i < each_phase_len_; ++i) {
+            sum += phase_state_[i] * coeff[i];
+        }
+        return sum;
+    }
+
+    void PushFirState(float x) noexcept {
+        for (int i = each_phase_len_ - 1; i > 0; --i) {
+            phase_state_[i] = phase_state_[i - 1];
+        }
+        phase_state_[0] = x;
+    }
+
+    int upsample_{};
+    int each_phase_len_{};
+    std::vector<float> phase_coeffs_;
+    std::vector<float> phase_state_;
+};
+}
--- tests/nogui/audiofx/resample.cpp
+++ tests/nogui/audiofx/resample.cpp
@@ -1,15 +1,21 @@
 #include "AudioFile.h"
-#include "qwqdsp/fx/resample_coeffs.h"
-#include "qwqdsp/fx/resample_iir.hpp"
 #include "work_dir.hpp"
+#include <qwqdsp/filter/window_fir.hpp>
+#include <qwqdsp/fx/polyphase_resample_fir.hpp>
+#include <qwqdsp/fx/resample.hpp>
+#include <qwqdsp/fx/resample_coeffs.h>
+#include <qwqdsp/fx/resample_iir.hpp>
+#include <qwqdsp/window/blackman_nuttall.hpp>
 
-int main() {
+
+constexpr float kTargetFs = 88200.0f;
+
+static void ResampleIir() {
     AudioFile<float> infile;
     infile.load(qwqdsp_support::InputFile("sweep.wav"));
     auto& sweep = infile.samples.front();
 
     qwqdsp_fx::ResampleIIR<qwqdsp_fx::coeff::BestCoeffs<float>, 128> resample;
-    constexpr float kTargetFs = 96000.0f;
     resample.Init(infile.getSampleRate(), kTargetFs);
     auto sweep_resample = resample.Process<float>(sweep);
 
@@ -19,5 +25,70 @@ int main() {
     outfile.setSampleRate(kTargetFs);
     outfile.setNumSamplesPerChannel(sweep_resample.size());
     outfile.samples.front() = std::move(sweep_resample);
-    outfile.save(qwqdsp_support::OutputFile("sweep_resample.wav"));
+    outfile.save(qwqdsp_support::OutputFile("sweep_resample_iir.wav"));
+}
+
+static void ResampleFir() {
+    AudioFile<float> infile;
+    infile.load(qwqdsp_support::InputFile("sweep.wav"));
+    auto& sweep = infile.samples.front();
+
+    qwqdsp_fx::Resample resample;
+    resample.Init(infile.getSampleRate(), kTargetFs, 180, 127, 128);
+    auto sweep_resample = resample.Process(sweep);
+
+    AudioFile<float> outfile;
+    outfile.setNumChannels(1);
+    outfile.setBitDepth(32);
+    outfile.setSampleRate(kTargetFs);
+    outfile.setNumSamplesPerChannel(sweep_resample.size());
+    outfile.samples.front() = std::move(sweep_resample);
+    outfile.save(qwqdsp_support::OutputFile("sweep_resample_fir.wav"));
+}
+
+static void ResamplePolyphaseFir() {
+    constexpr int coeff_len = 1024;
+    constexpr int ratio = 2;
+    float coeff[coeff_len];
+    qwqdsp_filter::WindowFIR::Lowpass(coeff, std::numbers::pi_v<float> / ratio);
+    qwqdsp_window::BlackmanNuttall::ApplyWindow(coeff, false);
+
+    AudioFile<float> infile;
+    infile.load(qwqdsp_support::InputFile("sweep.wav"));
+    auto& sweep = infile.samples.front();
+
+    {
+        qwqdsp_fx::PolyphaseDownsamplerFir resample;
+        resample.Init(coeff, ratio);
+        resample.Reset();
+        auto sweep_resample = resample.Process(sweep);
+
+        AudioFile<float> outfile;
+        outfile.setNumChannels(1);
+        outfile.setBitDepth(32);
+        outfile.setSampleRate(infile.getSampleRate() / ratio);
+        outfile.setNumSamplesPerChannel(sweep_resample.size());
+        outfile.samples.front() = std::move(sweep_resample);
+        outfile.save(qwqdsp_support::OutputFile("sweep_polyphase_resample_fir_down.wav"));
+    }
+    {
+        qwqdsp_fx::PolyphaseUpsamplerFir resample;
+        resample.Init(coeff, ratio);
+        resample.Reset();
+        auto sweep_resample = resample.Process(sweep);
+
+        AudioFile<float> outfile;
+        outfile.setNumChannels(1);
+        outfile.setBitDepth(32);
+        outfile.setSampleRate(infile.getSampleRate() * ratio);
+        outfile.setNumSamplesPerChannel(sweep_resample.size());
+        outfile.samples.front() = std::move(sweep_resample);
+        outfile.save(qwqdsp_support::OutputFile("sweep_polyphase_resample_fir_up.wav"));
+    }
+}
+
+int main() {
+    ResampleIir();
+    ResampleFir();
+    ResamplePolyphaseFir();
 }
