ccaa311cebfb53bbc56640177b8fc1521b4fa221
a5632645 <3022602213@qq.com>
2026-07-07 20:53:14 +0800

    fix: 导数窗计算表达式错误

--- include/qwqdsp/window/blackman_harris.hpp
+++ include/qwqdsp/window/blackman_harris.hpp
@@ -43,7 +43,6 @@ struct BlackmanHarris {
     }
 
     static void DWindow(std::span<float> x) noexcept {
-        constexpr float a0 = 0.35875f;
         constexpr float a1 = 0.48829f;
         constexpr float a2 = 0.14128f;
         constexpr float a3 = 0.01168f;
@@ -52,8 +51,9 @@ struct BlackmanHarris {
         size_t L = x.size();
         for (size_t n = 0; n < L; ++n) {
             float t = static_cast<float>(n) / static_cast<float>(L);
-            x[n] = a0 - twopi * a1 * std::cos(twopi * t) + twopi * 2 * a2 * std::cos(twopi * 2 * t)
-                 - a3 * twopi * 3 * std::cos(twopi * 3 * t);
+            x[n] = a1 * twopi * std::sin(twopi * t)
+                 - a2 * twopi * 2.0f * std::sin(twopi * 2.0f * t)
+                 + a3 * twopi * 3.0f * std::sin(twopi * 3.0f * t);
         }
     }
 private:
